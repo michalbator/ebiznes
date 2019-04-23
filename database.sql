efd419c1eda5d35c5feee7564de5489284264ea6
Michal <michal.bator@student.uj.edu.pl>
2019-04-23 10:19:31 +0200

    schemat bazy

---
+++ database.sql
@@ -0,0 +1,20 @@
+CREATE TABLE books (
+  id INT PRIMARY KEY,
+  isbn VARCHAR(13) NOT NULL,
+  title VARCHAR(100) NOT NULL,
+  author INT NOT NULL,
+  FOREIGN KEY (author) REFERENCES authors(id) ON DELETE CASCADE
+);
+
+CREATE TABLE authors (
+    id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    country INT,
+    FOREIGN KEY (country) REFERENCES countries(id) ON DELETE CASCADE
+);
+
+CREATE TABLE countries (
+    id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    short_name VARCHAR(10) NOT NULL
+);
